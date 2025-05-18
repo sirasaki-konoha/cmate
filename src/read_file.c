6a8aa80706d418b4b2284e82dea4c9d55cabdd98
noa-vliz <chocolatey.noa@gmail.com>
2025-05-19 00:45:39 +0900

    initial

---
+++ src/read_file.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void create_and_write(const char *path, const char *write) {
+  FILE *fp = fopen(path, "w+");
+
+  if (fp == NULL) {
+    fprintf(stderr, "Unable to write %s\n", path);
+    exit(EXIT_FAILURE);
+  }
+
+  fprintf(fp, "%s\n", write);
+  fclose(fp);
+}
